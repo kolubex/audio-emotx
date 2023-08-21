b06000613d33a06454b6024fb317a22149b3c967
kolubex <lakshmipathi.balaji@research.iiit.ac.in>
2023-08-21 23:03:17 +0530

    added_sbatch_scripts

--- run_emotx_audio.sh
+++ run_emotx_audio.sh
@@ -5,10 +5,12 @@
 #SBATCH -w gnode082
 #SBATCH --mem-per-cpu=2G
 #SBATCH --time=10-00:00:00
-#SBATCH --output=/home2/kolubex/audio_emotx/feat_extract/wavlm/logs/run_final_tt1568346.log
+#SBATCH --output=/home2/kolubex/audio_emotx/audio-emotx/logs/audio_emotx_run1.log
 #SBATCH --mail-user lakshmipathi.balaji@research.iiit.ac.in
 #SBATCH --mail-type ALL
 
 cd /home2/kolubex/audio_emotx/audio-emotx/
+conda init
+conda activate emotx
 git checkout audio_integration
 python trainer.py
