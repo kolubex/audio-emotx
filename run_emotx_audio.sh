9d0da8c97dfb8e5bd6cf34af6a531d434eee2f42
kolubex <lakshmipathi.balaji@research.iiit.ac.in>
2023-09-02 12:21:55 +0530

    wavlm_pretrained

--- run_emotx_audio.sh
+++ run_emotx_audio.sh
@@ -2,10 +2,10 @@
 #SBATCH -A kolubex
 #SBATCH -n 10
 #SBATCH --gres=gpu:1
-#SBATCH -w gnode082
+#SBATCH -w gnode053
 #SBATCH --mem-per-cpu=2G
 #SBATCH --time=10-00:00:00
-#SBATCH --output=/home2/kolubex/audio_emotx/audio-emotx/logs/audio_emotx_run1.log
+#SBATCH --output=/home2/kolubex/audio_emotx/audio-emotx/logs/audio_emotx_run4.log
 #SBATCH --mail-user lakshmipathi.balaji@research.iiit.ac.in
 #SBATCH --mail-type ALL
 
